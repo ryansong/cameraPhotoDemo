4ca7ce2624217904a951352d6626f7e1c6197a2e
Firstname Lastname <song.ryan90@gmail.com>
2014-04-09 16:08:24 +0800

    Initial Commit

---
+++ cameraPhotoDemo/SYBViewController.h
@@ -0,0 +1,13 @@
+//
+//  SYBViewController.h
+//  cameraPhotoDemo
+//
+//  Created by Song Xiaoming on 4/9/14.
+//  Copyright (c) 2014 Song Xiaoming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SYBViewController : UIViewController
+
+@end
